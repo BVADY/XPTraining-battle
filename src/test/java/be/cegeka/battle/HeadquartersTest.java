4d029955672313b6c98b03f0e3294eb90866448b
Emma <Lindert_vdp@hotmail.com>
2018-05-24 13:22:20 +0200

    US4 enroll soldiers in army

--- src/test/java/be/cegeka/battle/HeadquartersTest.java
+++ src/test/java/be/cegeka/battle/HeadquartersTest.java
@@ -1,14 +1,13 @@
 package be.cegeka.battle;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 public class HeadquartersTest {
 
     @Test
-    public void testReportCasualty()
+    public void testReportCasualty() {
+
+    }
 
 
 }
